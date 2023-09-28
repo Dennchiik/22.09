d1a78ae129b30b631a07214bc7b0d62aad6910a6
Dennchiik <119806784+Dennchiik@users.noreply.github.com>
2023-09-28 22:53:06 +0300

    Add files via upload

---
+++ 22.09(2)/22.09(2)/22.09(2).cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#define SUM_UP_TO_N(n) ((n * (n + 1)) / 2)
+
+int main() {
+    int n = 10; 
+
+    int sum = SUM_UP_TO_N(n);
+
+    std::cout << "Сумма чисел от 1 до " << n << " равна " << sum << std::endl;
+
+    return 0;
+}
---
+++ 22.09(3)/22.09(3)/22.09(3).cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#define POWER(base, exponent) ({ \
+    typeof(base) result = 1;    \
+    for (int i = 0; i < (exponent); i++) { \
+        result *= (base); \
+    } \
+    result; \
+})
+
+int main() {
+    double base = 2.0;
+    int exponent = 3;
+
+    double result = POWER(base, exponent);
+
+    std::cout << base << " в степени " << exponent << " равно " << result << std::endl;
+
+    return 0;
+}
